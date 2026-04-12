1659c252b1872592260ecb3531c2487dec23ab11
Ryan Zomparelli <zomp.ryan@gmail.com>
2026-04-12 12:35:34 -0400

    feat(hooks): add PizzaOfTheDay component with custom hook.

---
+++ src/hooks/usePizzaOfTheDay.js
@@ -0,0 +1,26 @@
+import { useState, useEffect, useDebugValue } from "react";
+
+const usePizzaOfTheDay = () => {
+  const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
+
+  // Debugging technique made especially for custom hooks:
+  useDebugValue(pizzaOfTheDay ? `${pizzaOfTheDay.name}` : "Loading...");
+  // Now open your React Dev Tools and inspect our PizzaOfTheDay component.
+  // You'll see our debug value there. This is helpful when you have lots of custom
+  // hooks and in particular lots of reused custom hooks that have differing values.
+  // It can help at a glance to discern which hook has which data inside of it.
+
+  useEffect(() => {
+    async function fetchPizzaOfTheDay() {
+      const res = await fetch("/api/pizza-of-the-day");
+      const data = await res.json();
+      setPizzaOfTheDay(data);
+    }
+
+    fetchPizzaOfTheDay();
+  }, []);
+
+  return pizzaOfTheDay;
+};
+
+export default usePizzaOfTheDay;
